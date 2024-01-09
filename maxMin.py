ee87662142a5a0e053a1908b19a281e5e82306e6
kurves <kavssimply@gmail.com>
2024-01-09 22:46:30 +0300

    function to get max and min sum values

--- maxMin.py
+++ maxMin.py
@@ -1,5 +1,9 @@
 #function to get max and min sum values
 def maxMin(arr):
+    num = 4
+    for i in arr:
+        if i == 4:
+            break
     sum =0
     for i in arr:
         for j in arr:
