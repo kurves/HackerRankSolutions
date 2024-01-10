12a1cd23b3202d47925c80059ce7ed5eb0b7d024
kurves <kavssimply@gmail.com>
2024-01-10 21:12:00 +0300

    Return the correct max nad min values

--- maxMin.py
+++ maxMin.py
@@ -4,9 +4,5 @@ def maxMin(arr):
    min_sum = sum(arr[:-1])
    max_sum = sum(arr[1:])
    return min_sum, max_sum
-    sum =0
-    for i in arr:
-        for j in arr:
-            total = sum([i][j])
-            print(total)
+   
 
