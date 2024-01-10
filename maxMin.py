9453474bbf7edc9fb47a2fbe37dd8a7dc3ddde3d
kurves <kavssimply@gmail.com>
2024-01-10 21:10:23 +0300

    function to get min sum

--- maxMin.py
+++ maxMin.py
@@ -1,12 +1,7 @@
 #function to get max and min sum values
-def maxMin(nums, target):
-    valuesIndices ={}
-    for i, num in enumerate(nums):
-        com = target - num
-    num = 4
-    for i in arr:
-        if i == 4:
-            break
+def maxMin(arr):
+   arr.sort()
+   min_sum = sum(arr[:-1])
     sum =0
     for i in arr:
         for j in arr:
