37110be5a47a83346a39d5c32ec9bf6395693b97
kurves <kavssimply@gmail.com>
2024-01-12 21:25:07 +0300

    Fix compilatio error

--- plusMinus.py
+++ plusMinus.py
@@ -1,6 +1,6 @@
 def plusMinus(arr):
     """funcion to calculate the ratios of integers that are positive negative and zero"""
-    numElements = arr.length 
-    print(arr.length)
+    numElements = len(arr)
+    print(numElements)
 
 plusMinus([1,2,3])
