dec275b69199d52ac2d19ea7beee41328eec2a45
kurves <kavssimply@gmail.com>
2024-01-12 21:29:15 +0300

    function to get ratios of negative and positives

--- plusMinus.py
+++ plusMinus.py
@@ -1,6 +1,10 @@
 def plusMinus(arr):
     """funcion to calculate the ratios of integers that are positive negative and zero"""
     numElements = len(arr)
+    posNumber = len([ i for i in arr if i >0])
+    negNumber = len([i for i in arr if i < 0])
+    zeroNumber = len([i for i in arr if i ==0])
+    print(posNumber)
     print(numElements)
 
 plusMinus([1,2,3])
