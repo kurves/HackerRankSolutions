f826f5055f3045113b1e5537a00fe0b8d58cf144
kurves <kavssimply@gmail.com>
2024-01-12 21:30:45 +0300

    get the number of positive and negative integers

--- plusMinus.py
+++ plusMinus.py
@@ -5,6 +5,8 @@ def plusMinus(arr):
     negNumber = len([i for i in arr if i < 0])
     zeroNumber = len([i for i in arr if i ==0])
     print(posNumber)
+    print(negNumber)
+    print(zeroNumber)
     print(numElements)
 
-plusMinus([1,2,3])
+plusMinus([1,2,3, -5, -6, 0, 0])
