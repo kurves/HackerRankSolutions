c549c8be111539059d06e69c5ef089075043d4e0
kurves <kavssimply@gmail.com>
2024-01-13 20:25:27 +0300

    function to get average

--- plusMinus.py
+++ plusMinus.py
@@ -1,9 +1,9 @@
 def plusMinus(arr):
     """funcion to calculate the ratios of integers that are positive negative and zero"""
     numElements = len(arr)
-    posNumber = len([ i for i in arr if i >0])
-    negNumber = len([i for i in arr if i < 0])
-    zeroNumber = len([i for i in arr if i ==0])
+    posNumberAve = len([ i for i in arr if i >0]) / numElements
+    negNumberAve = len([i for i in arr if i < 0]) / numElements
+    zeroNumberAve = len([i for i in arr if i ==0])  / numElements
     print(posNumber)
     print(negNumber)
     print(zeroNumber)
