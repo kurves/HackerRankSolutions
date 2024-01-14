7a12264f61fc402a268cbcabaa9a51a4a4bfc62c
kurves <kavssimply@gmail.com>
2024-01-14 19:10:26 +0300

    funcion to convert time

--- timeConversion.py
+++ timeConversion.py
@@ -1,7 +1,7 @@
 # function to convert from 12 hour to 24hr
 def timeConversion(s):
     clocksystem = s[-2:]
-    if clocksystem == 'PM':
+    if clocksystem == 'PM' and s[:2] != "12":
         clock24 = int(s[:2]) + 12 
         clock245 = str(clock24) + s[2:-2]
         print(clock245)
@@ -10,8 +10,11 @@ def timeConversion(s):
         fomClock12 = "{:02}".format(clock12)
         clock124 = str(fomClock12) + s[2:-2]
         print(clock124)
+    elif s[:2] == '12':
+        print(s[:-2])
 
-    #if s[-1]
+  
 
 timeConversion("07:05:45PM")
 timeConversion("12:01:00AM")
+timeConversion("12:01:00PM")
