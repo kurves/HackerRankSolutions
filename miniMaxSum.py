a9eb808730dc5672efac1b7cf6381dcf73a1e5c4
kurves <kavssimply@gmail.com>
2024-01-14 18:32:36 +0300

    get minimum sum

--- miniMaxSum.py
+++ miniMaxSum.py
@@ -2,9 +2,10 @@ import random
 import math
 def miniMaxSum(arr):
     sorted_arr = sorted(arr)
+    min_sum = sum(arr[1:])
     four_num = random.sample(arr,4)
     min_sum = sum(four_num)
-   
+    print(min_sum)
     print(sorted_arr)
 
 miniMaxSum([1,5,7,9,3])
