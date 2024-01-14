cd8414ac5b70b1ba33f3cec973708dce001f2e93
kurves <kavssimply@gmail.com>
2024-01-14 18:37:03 +0300

    function toget maximum and min sum

--- miniMaxSum.py
+++ miniMaxSum.py
@@ -4,8 +4,6 @@ def miniMaxSum(arr):
     sorted_arr = sorted(arr)
     min_sum = sum(sorted_arr[:-1])
     max_sum = sum(sorted_arr[1:])
-    four_num = random.sample(arr,4)
-    min_sum = sum(four_num)
     print(min_sum, max_sum)
     print(sorted_arr)
 
