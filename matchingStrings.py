32f9113f09e5c828d5c8029fa5791b34db7ceb73
kurves <kavssimply@gmail.com>
2024-01-16 21:45:13 +0300

    fucntion to get matching strings

--- matchingStrings.py
+++ matchingStrings.py
@@ -4,7 +4,10 @@ def matchingStrings(strings,queries):
     for i in queries:
         if i in strings:
             c = strings.count(i)
-        print(c)
+            print(c)
+        elif i not in strings:
+            c = 0    
+    
        
            
 matchingStrings(['ab','ab','abc'],['ab','abc','bc'])
