bc4db3908d8b328e731e049dcfa6eb58129807e7
kurves <kavssimply@gmail.com>
2024-01-16 21:13:53 +0300

    fucntion to get unique integer

---
+++ lonelyInteger.py
@@ -0,0 +1,8 @@
+def lonelyInteger(a):
+    unique_set = set(a)
+    print(unique_set)
+    print(unique_set^a)
+    print(unique_set&a)
+    print(unique_set-a)
+
+lonelyInteger([1,2,3,4,3,2,1])    
--- matchingStrings.py
+++ matchingStrings.py
@@ -2,9 +2,10 @@ from collections import Counter
 def matchingStrings(strings,queries):
     counts  ={}
     for i in strings:
-        if i in  queries:
-            counts[i] += 1
-        else:
-            print(counts =0)
+        if i in counts:
+            count = queries.count(i)
+            counts[i] += count
+            print(count)
+       
            
 matchingStrings(['ab','ab','abc'],['ab','abc','bc'])
