73665c467695fea615c033b4d5be1c808b489385
kurves <kavssimply@gmail.com>
2024-01-16 21:29:47 +0300

    function to get lonely integer

--- lonelyInteger.py
+++ lonelyInteger.py
@@ -1,8 +1,10 @@
 def lonelyInteger(a):
-    unique_set = set(a)
-    print(unique_set)
-    print(unique_set^a)
-    print(unique_set&a)
-    print(unique_set-a)
+    for i in a:
+        n = a.count(i)
+        if n == 1:
+            print(a[i])
+      
+
+    
 
 lonelyInteger([1,2,3,4,3,2,1])    
