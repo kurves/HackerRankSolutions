d2bd33feced55a85493d264a33328bd62ab51e02
kurves <kavssimply@gmail.com>
2024-01-31 12:15:55 +0300

    function to check leap year

--- leap.py
+++ leap.py
@@ -4,10 +4,8 @@ def leap(year):
     div_100 = (year % 100 == 0)
     div_400 = (year % 400 == 0)
     if div_4 and (div_400 or div_100):
-        print("t")
         return True
-    elif div_4 and div_100:
-        print("f")
+    else:
         return False
 
         
