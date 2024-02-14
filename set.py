1f89c9525fd74b9081dd1bb0f986b17fbce55973
kurves <kavssimply@gmail.com>
2024-02-14 17:33:43 +0300

    function to check foe subset of set

--- set.py
+++ set.py
@@ -1,9 +1,9 @@
 #function to look for subset of a set
 
 
-seta= set(map(int, input).split())
-setb = set(map(int, input).split())
-if seta.issubset(b):
+seta= set(map(int, input().split()))
+setb = set(map(int, input().split()))
+if seta.issubset(setb):
     print("True")
 else:
     print("False")
