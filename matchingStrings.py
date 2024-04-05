5c14b1898e93fd585006ab5f49e2f7e1a32088de
kurves <kavssimply@gmail.com>
2024-04-05 19:25:22 +0300

    algorithm to match queries to strings

--- matchingStrings.py
+++ matchingStrings.py
@@ -1,12 +1,10 @@
-from collections import Counter
 def matchingStrings(strings,queries):
     counts  ={}
     for i in queries:
         if i in strings:
             c = strings.count(i)
             print(c)
-        elif i not in strings:
-            c = 0    
+       
     
        
            
