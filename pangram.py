d894403e3741612e699350c8429d38d284bb477c
kurves <kavssimply@gmail.com>
2024-04-07 21:53:34 +0300

    algorithm to get pangram

--- pangram.py
+++ pangram.py
@@ -2,6 +2,8 @@ def pangrams(s):
     # pangram string that contains every letter of the alphabet
     new_s = ''.join(s.split())
     print(new_s)
+    for i in range(ord('a'), ord('z')):
+        print(i)
 
 
 pangrams('This is a laxy fox dog')
