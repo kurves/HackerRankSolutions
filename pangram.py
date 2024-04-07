1d161fd2bef7a628f80b2545fec9d92da415ab67
kurves <kavssimply@gmail.com>
2024-04-07 20:28:51 +0300

    scaript to get a pangram

--- pangram.py
+++ pangram.py
@@ -1,6 +1,7 @@
 def pangrams(s):
     # pangram string that contains every letter of the alphabet
-    s.strip().join()
-    print(s)
+    new_s = ''.join(s.split())
+    print(new_s)
 
 
+pangrams('This is a laxy fox dog')
