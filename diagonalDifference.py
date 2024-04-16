448f28ca1c09b2b46e1ccd8c9a29b2a845abbec1
kurves <kavssimply@gmail.com>
2024-04-16 12:52:19 +0300

    algorithm to get diagonal differences

--- diagonalDifference.py
+++ diagonalDifference.py
@@ -6,3 +6,7 @@ def diagonalDifference(arr):
 
     for i in range(len(arr)):
         left_diagonal_sum += arr[i][i]
+
+        right_diagonal_sum += arr[i][len(arr) - i - 1]
+
+        diagonal_difference = abs(left_diagonal_sum - right_diagonal_sum)
